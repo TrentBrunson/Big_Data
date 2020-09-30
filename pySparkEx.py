aace361d474ba1ec49e51cfd5b509153bef2e7ed
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-09-29 19:17:14 -0500

    setting up PySpark env

---
+++ pySparkEx.py
@@ -0,0 +1,25 @@
+#%%
+import os
+#%%
+# Find the latest version of spark 3.0  from http://www-us.apache.org/dist/spark/ and enter as the spark version
+# For example:
+# spark_version = 'spark-3.0.1'
+spark_version = 'spark-3.0.1'
+os.environ['SPARK_VERSION']=spark_version
+#%%
+# Install Spark and Java
+!apt-get update
+!apt-get install openjdk-11-jdk-headless -qq > /dev/null
+!wget -q http://www-us.apache.org/dist/spark/$SPARK_VERSION/$SPARK_VERSION-bin-hadoop2.7.tgz
+!tar xf $SPARK_VERSION-bin-hadoop2.7.tgz
+!pip install -q findspark
+#%%
+# Set Environment Variables
+import os
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
+os.environ["SPARK_HOME"] = f"/content/{spark_version}-bin-hadoop2.7"
+#%%
+# Start a SparkSession
+import findspark
+findspark.init()
+# %%
